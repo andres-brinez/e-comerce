2ab553e4058b7ce50e974a461d2d6445c22c3460
andres briñez <brinezlopez08@gmail.com>
2022-04-28 23:56:06 -0500

    .organizar archivos

--- js/json/leeer_datos.js
+++ js/json/leeer_datos.js
@@ -1,14 +1,27 @@
-console.log(1)
+import {  mostrar_datos } from "./mostrar-datos.js";
 
 //enviar la peticion al servidor
-const http=new XMLHttpRequest()
+const http = new XMLHttpRequest()
 http.open('GET', 'https://andres-brinez.github.io/json-ecomerce/informacion.json')
 http.send();
 
 
 //para ver  la respuesta del servidor  a la peticion cuando cargue 
-http.onload= ()=>{
-    const data =http.response  //trae la respuesta del  servidor
-    console.log(data)
-   
+http.onload = () => {
+    //lo que recibimos del servidor es texto,por eso debemos pasarlo a algo que  js pueda manejas (array)
+    //eso se hace con JSON.parse
+    const data = JSON.parse(http.response) //trae la respuesta del  servidor
+
+    const contenedor_start_wars = document.querySelector("[start-wars]")
+    const informacion_start_wars = data.start_wars
+    mostrar_datos(informacion_start_wars,contenedor_start_wars)
+
+    const informacion_consolas = data.consolas
+    const contenedor_consolas = document.querySelector("[consolas]")
+    mostrar_datos(informacion_consolas,contenedor_consolas)
+
+    const informacion_diversos = data.diversos
+    const contenedor_diversos = document.querySelector("[diversos]")
+    mostrar_datos(informacion_diversos,contenedor_diversos)
+
 }
---
+++ js/json/mostrar-datos.js
@@ -0,0 +1,22 @@
+const estructara_producto = (nombre, precio, imagen) => {
+    const contenedor = document.createElement("div")
+    contenedor.classList.add("contenedor-producto")
+    const contenido = `
+            <img src=${imagen} />
+            <p class="name-product">${nombre}</p>
+            <p class="price-product">${precio}</p>
+            <a href="./producto.html" class="see-product" target="_blank">Ver producto</a>
+    `
+    contenedor.innerHTML = contenido;
+    return contenedor;
+}
+
+
+export function mostrar_datos(informacion_producto,contenedor_producto){
+   
+    for (let i = 0; i < informacion_producto.length; i++) {
+        const elemento = informacion_producto[i];
+        const estructura_elemento = estructara_producto(elemento.nombre, elemento.precio, elemento.imagen)
+        contenedor_producto.appendChild(estructura_elemento)
+    }
+}
