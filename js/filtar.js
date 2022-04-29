508921b6d07d7de9dba9481dd07504301bf021e5
andres briñez <brinezlopez08@gmail.com>
2022-04-29 02:01:25 -0500

    .sd

--- js/filtar.js
+++ js/filtar.js
@@ -12,7 +12,7 @@ campo_filtrar.addEventListener("input", () => {
             let producto = productos[i];
             let nombre_producto = producto.querySelector(".title-type-product").textContent
             //i no diferencia entre mayusculas y  minusculas
-            let regex = new RegExp(campo_filtrar.value,"i")
+            let regex = new RegExp(campo_filtrar.value, "i")
 
             //revisa  si la letra que se  ingresa esta en el nombre, si esta devuelve true 
             if (!regex.test(nombre_producto)) {
@@ -27,13 +27,16 @@ campo_filtrar.addEventListener("input", () => {
 
     }
 
-    else{
+    else {
         for (let i = 0; i < productos.length; i++) {
             let producto = productos[i];
             producto.classList.remove("invisible")
-            }
-      }
+        }
+    }
 
 
 
 })
+
+
+
