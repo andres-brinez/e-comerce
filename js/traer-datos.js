066e37ef06df3dfe4adbdc787fa3beae3062f591
andres briñez <brinezlopez08@gmail.com>
2022-05-18 20:15:30 -0500

    cambios

--- js/traer-datos.js
+++ js/traer-datos.js
@@ -23,6 +23,7 @@ informacion_diversos.send();
 const url = new URL(window.location);
 
 
+
 //para ver  la respuesta del servidor  a la peticion cuando cargue 
 informacion_start_wars.onload = () => {
 
@@ -31,17 +32,17 @@ informacion_start_wars.onload = () => {
     const datos_start_wars = JSON.parse(informacion_start_wars.response) //trae la respuesta del  servidor
     const contenedor_start_wars = document.querySelector("[start-wars]")
 
-    if (url.pathname == '/index.html' || url.pathname == '/e-comerce/' ){
-        informacion_producto(datos_start_wars, contenedor_start_wars,'start wars')
-        
+    if (url.pathname.includes('index') || url.pathname == '/e-comerce/') {
+        informacion_producto(datos_start_wars, contenedor_start_wars, 'start wars')
+
     }
-    
-    else if (url.pathname == '/all-productos.html'){
+
+    else if (url.pathname.includes('/all-productos.html')) {
         informacion_productos(datos_start_wars)
 
     }
 
-    else if (url.pathname == '/inicio-administrador.html'){
+    else if (url.pathname.includes('/inicio-administrador.html')) {
         informacion_productos_administrador(datos_start_wars)
     }
 
@@ -51,15 +52,15 @@ informacion_consolas.onload = () => {
 
     const datos_consolas = JSON.parse(informacion_consolas.response)
     const contenedor_consolas = document.querySelector("[consolas]")
-    if (url.pathname == '/index.html' || url.pathname == '/e-comerce/' ){
+    if (url.pathname.includes('index')|| url.pathname == '/e-comerce/') {
         informacion_producto(datos_consolas, contenedor_consolas, 'consolas')
     }
 
-    else if (url.pathname == '/all-productos.html') {
+    else if (url.pathname.includes('/all-productos.html')) {
         informacion_productos(datos_consolas)
     }
 
-    else if (url.pathname == '/inicio-administrador.html') {
+    else if (url.pathname.includes('/inicio-administrador.html')) {
         informacion_productos_administrador(datos_consolas)
     }
 
@@ -71,18 +72,18 @@ informacion_diversos.onload = () => {
     const datos_diversos = JSON.parse(informacion_diversos.response)
     const contenedor_diversos = document.querySelector("[diversos]")
 
-    if (url.pathname == '/index.html' || url.pathname == '/e-comerce/' ){
+    if (url.pathname.includes('index')|| url.pathname == '/e-comerce/') {
         informacion_producto(datos_diversos, contenedor_diversos)
 
     }
 
-    else if (url.pathname == '/all-productos.html') {
+    else if (url.pathname.includes('/all-productos.html')) {
 
         informacion_productos(datos_diversos)
 
     }
 
-    else if (url.pathname == '/inicio-administrador.html') {
+    else if (url.pathname.includes('/inicio-administrador.html')) {
         informacion_productos_administrador(datos_diversos)
     }
 
