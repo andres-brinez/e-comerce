fe34e38a8e7bb05a0fdaebd4383ef5ad5fe1eebf
andres briñez <brinezlopez08@gmail.com>
2022-05-18 21:35:52 -0500

    cambiar validaciones

--- js/validaciones/validar-login.js
+++ js/validaciones/validar-login.js
@@ -5,7 +5,9 @@ const input_password = document.querySelector('#login-password');
 button_login.addEventListener('click', (evento) => {
     evento.preventDefault();
     const emailRegex = /^[-\w.%+]{1,64}@(?:[A-Z0-9-]{1,63}\.){1,125}[A-Z]{2,63}$/i;
-    console.log(emailRegex.test(input_email.value))
+    const nombre_usuario = document.querySelector('#login-email')
+    const contraseña_usuario = document.querySelector('#login-password')
+
     if (input_email.value === '' || input_password.value === '') {
 
         Swal.fire({
@@ -27,9 +29,32 @@ button_login.addEventListener('click', (evento) => {
 
     }
 
-    else{
-        window.location.replace("./inicio-administrador.html");   
+    else if (contraseña_usuario.value.length<5){
+        Swal.fire({
+            icon: 'error',
+            title: 'Oops...Error',
+            text: 'contraseña invalida '
+            ,
+
+        })
     }
+
+    else { 
+        console.log(nombre_usuario.value)
+        console.log(contraseña_usuario.value)
+        if (nombre_usuario.value=='brinezlopez08@gmail.com'  && contraseña_usuario.value=='comedor20'  ) {
+            window.location.replace("./inicio-administrador.html?administrador=True");
+            
+        }
+
+        else {
+            window.location.replace("./inicio-administrador.html?administrador=False");
+        }
+
+        }
+
+        
+    
 })
 
 
