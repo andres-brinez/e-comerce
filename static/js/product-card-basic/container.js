cd645370654189c49d95dc5454ed536cf2138786
Andres Briñez <brinezlopez08@gmail.com>
2023-01-28 17:32:18 -0500

    Dividir código

---
+++ static/js/product-card-basic/container.js
@@ -0,0 +1,50 @@
+// se encarga de  la estructura del contendor de los productos 
+
+function createContainerArticle(key){
+    const containerMain = document.createElement('article');
+    containerMain.classList.add("article-type-product")
+    containerMain.setAttribute("category", key)
+    containerMain.innerHTML=
+    
+    `
+
+        <div class="descripcion">
+          <h2 class="title-type-product">${key}</h2>
+          <div class="see-all">
+            <a href="index.html#allProducts">ver todo</a><img src="./static/img/section-products/flecha.svg" alt="flecha" class="flecha" />
+          </div>
+        </div>
+        <div class="contenedor-produtos " ${key}> 
+        </div>
+    
+    `
+
+    sectionProducts.appendChild(containerMain)
+
+}
+
+function createContainerAllProducts(){
+
+    const container = document.createElement('section');
+    container.classList.add("section-products")
+
+    container.innerHTML=`
+        <article class="article-type-product" >
+            <div class="descripcion titulo">
+            <h2 class="title-type-product">Todos los productos</h2>
+            </div>
+            <div class="contenedor-produtos" id='all-products' contenedor-productos> 
+            </div>
+        </article>
+    `
+
+    // borra todo lo  que haya en el main 
+    main.innerHTML=''
+
+    main.appendChild(container)
+ 
+
+}
+
+
+export {createContainerAllProducts,createContainerArticle}
---
+++ static/js/product-card-basic/estructura_producto.js
@@ -0,0 +1,18 @@
+const estructura_producto = (imagen,nombre,precio,id) => {
+    
+    const contenedor = document.createElement("div")
+    contenedor.classList.add("contenedor-producto")
+    const contenido = `
+            <img src=${imagen} />
+            <p class="name-product">${nombre}</p>
+            <p class="price-product">${precio}</p>
+            <a href="./producto.html?id=${id}" class="see-product" target="_blank">Ver producto</a>
+    `
+    contenedor.innerHTML = contenido;
+    return contenedor;
+}
+
+export {estructura_producto} 
+    
+
+
