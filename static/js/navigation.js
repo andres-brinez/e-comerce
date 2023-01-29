99ce3a5b48db01a1c2d9409da09d5f8b895f5300
Andres Briñez <brinezlopez08@gmail.com>
2023-01-28 22:11:02 -0500

    Navegación para ver  todos  los  productos  y  un productp seleccioando

--- static/js/navigation.js
+++ static/js/navigation.js
@@ -1,13 +1,13 @@
 import { data } from "../../Information/data.js";
-import { informacion_basic_producto } from "./mostrar-datos-inicio.js";
+import { informacion_basic_producto } from "./product-card-basic/index.js";
+import { mostrarProducto } from "./InformationProducto/index.js";
 
-import { informacion_productos } from "./mostrar all productos/mostrar-datos.js";
-import { informacion_productos_administrador } from "./mostrar-datos-administrador/mostrar-datos.js"
-
-
+// import { informacion_productos_administrador } from "./mostrar-datos-administrador/mostrar-datos.js"
 
 //new URL es una funcion ya echa que nos trae la url
 const url = new URL(window.location);
+
+console.log(url);
 
 // home
 if (url.hash===''){
@@ -16,14 +16,21 @@ if (url.hash===''){
     
 }
 
+else if (url.hash==='#allProducts'){
+    console.log('allProducts')
+    informacion_basic_producto(data,'all-products')
+}
+
+else if (url.hash.includes('productoID')) {
+    console.log('producto')
+    buttonRegister.style.display = 'none'
+
+    mostrarProducto(url,data)
+
+}
+
 // else if (url.pathname.includes('/inicio-administrador.html')) {
 //     informacion_productos_administrador(informacion_start_wars)
 //     informacion_productos_administrador(informacion_consolas)
 //     informacion_productos_administrador(informacion_diversos)
 
-
-// if (url.pathname.includes('/all-productos.html')) {
-
-//     informacion_productos(data.consolas)
-
-// }
