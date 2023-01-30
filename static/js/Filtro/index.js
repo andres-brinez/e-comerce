d26c967fb5320343a352ac89a477d830a736bf3f
Andres Briñez <brinezlopez08@gmail.com>
2023-01-29 21:55:58 -0500

    Funcion  que se encarga de hacer el filtro de  busqueda

---
+++ static/js/Filtro/index.js
@@ -0,0 +1,43 @@
+import { findProduct } from "./findProduct.js";
+import { AddProductStructure } from "./estructura.js";
+
+
+lupaIcon.addEventListener("click", ()=>{
+
+    const coincidencias = findProduct()
+
+    if (coincidencias.length == 0){
+        Swal
+            .fire({
+                title: "Lo sentimos",
+                text: `No hay resultados para la busqueda ${campo_filtrar.value} `,
+                icon: "info",
+                button: "Ok!",
+            })
+            // .then(resultado => {
+            //     //cuando cierre el mensaje en ok
+            //     if (resultado.value) {
+            //         location.reload();
+                    
+            //     } 
+            // });
+    }
+    else {
+        //Crea la estructura en html para ver  los productos que coinciden con lo ingresado 
+        AddProductStructure(coincidencias)
+    }
+
+})
+
+
+
+
+
+
+    //         
+    //     }
+    // }
+    // }
+
+   
+
