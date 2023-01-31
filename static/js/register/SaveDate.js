0c9310d528bea96f6e5220c1372e29714b691817
Andres Briñez <brinezlopez08@gmail.com>
2023-01-31 14:06:50 -0500

    Guardar  los datos en el localStorage

---
+++ static/js/register/SaveDate.js
@@ -0,0 +1,25 @@
+// guarda los datos en el localStorage
+function saveData(data) {
+
+    try{
+         // local storage solo funciona en el navegador de  la persona
+        // setItem - agregar  la informacion  al local storage
+        // stringify pasa los datos del objeto javascript a string
+        localStorage.setItem(data.email, JSON.stringify(data.password))
+        return true
+
+    }
+    catch(err){
+        console.log("Error al guardar datos",err)
+        
+    }
+   
+
+}
+
+export  {saveData}
+// function getData() {
+//     return JSON.parse(localStorage.getItem('data'));
+// }
+
+
