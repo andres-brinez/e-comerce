c517a5874bff9dfd93f4b39bf732c2a7cf032533
Andres Briñez <brinezlopez08@gmail.com>
2023-01-31 14:05:05 -0500

    Validar los datos del formulario

--- static/js/register/index.js
+++ static/js/register/index.js
@@ -1,10 +1,14 @@
 import { structure } from "./structure.js"
+import { validationForm } from "./ValidacionesForm.js"
 
 
 function register(){
 
     // Crea la estructura del formulario de registro
     structure()
+    
+    // Crea los eventos del formulario de registro
+    validationForm()
 
 }
 
