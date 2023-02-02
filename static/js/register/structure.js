2030c0b7326440d835842530314c31f7f4264b64
Andres Briñez <brinezlopez08@gmail.com>
2023-02-02 12:20:19 -0500

    Agregar valores al select

--- static/js/register/structure.js
+++ static/js/register/structure.js
@@ -6,8 +6,9 @@ function structure(){
 
     <h2>Registrarce</h2>
             <form action="" class="formulario-login">
-                <input id="text" type="email" placeholder="Nombre">
-                <select name="selectRol" id="">
+                <input id="nameUser" type="text" placeholder="Nombre">
+                <select name="selectRol" id="selectRol" required>
+                    <option value="">Elige tu  rol</option>
                     <option value="user">Usuario</option>
                     <option value="administrador">Administrador</option>
                 </select>
