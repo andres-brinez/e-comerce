816f725997bc863adca6e4fd15642182ef612e31
Andres Briñez <brinezlopez08@gmail.com>
2023-02-01 23:27:22 -0500

    Crea la estructura del login

---
+++ static/js/login/structure.js
@@ -0,0 +1,20 @@
+function structureLogin(){
+
+    const container= document.createElement('div');
+    container.classList.add('login');
+
+    container.innerHTML=`
+    <h2>Iniciar Sesión</h2>
+    <form action="" class="formulario-login">
+        <input id="login-email" type="email" placeholder="Escriba su correo electrónico">
+        <input id="login-password" type="password" placeholder="Escriba su contraseña">
+        <button id="login-buttom" type="submit" >Entrar </button>
+    </form>
+    `
+
+    main.innerHTML=''
+    main.appendChild(container);
+
+}
+
+export {structureLogin}
