f57db1b9fe7b07db5ab8d05f411b5d2ed26585ba
Andres Briñez <brinezlopez08@gmail.com>
2023-02-02 12:19:59 -0500

    Agregar datos a guardar en el localStorage

--- static/js/register/SaveDate.js
+++ static/js/register/SaveDate.js
@@ -5,12 +5,13 @@ function saveData(data) {
          // local storage solo funciona en el navegador de  la persona
         // setItem - agregar  la informacion  al local storage
         // stringify pasa los datos del objeto javascript a string
-        localStorage.setItem(data.email, JSON.stringify(data.password))
+        localStorage.setItem(data.email, JSON.stringify(data))
         return true
 
     }
     catch(err){
         console.log("Error al guardar datos",err)
+        alert("Error",err)
         
     }
    
