dcd251f8e7e284443b3576bdb994f167e8d1c4eb
Andres Briñez <brinezlopez08@gmail.com>
2023-02-04 22:01:47 -0500

    navegación para agregar productos

--- static/js/navigation.js
+++ static/js/navigation.js
@@ -3,67 +3,120 @@ import { informacion_basic_producto } from "./product-card-basic/index.js";
 import { mostrarProducto } from "./InformationProducto/index.js";
 import { register } from "./register/index.js";
 import { login } from "./login/index.js";
+import { dashboard } from "./dashboard/index.js";
+import { addProduct } from "./dashboard/agregar-productos/addProduct.js";
+import { validarArrastrarImagen } from "./dashboard/agregar-productos/validaciones/validar-arrastrar-imagen.js";
+import { inputFileValidate } from "./dashboard/agregar-productos/validaciones/validar-button-img.js";
 
-// import { informacion_productos_administrador } from "./mostrar-datos-administrador/mostrar-datos.js"
 
 //new URL es una funcion ya echa que nos trae la url
 const url = new URL(window.location);
 
 console.log(url);
 
-window.addEventListener('hashchange',() =>{
+window.addEventListener('hashchange', () => {
     location.reload()
     window.scrollTo(0, 0); /* lleva  al inicio de la página */
 });
 
 // al recargar pagina
-window.onload = ()=> {
+window.onload = () => {
     navigation()
 }
 
-function navigation (){
+function navigation() {
     // home
-    if (url.hash===''){
+    if (url.hash === '') {
         console.log('inicio')
         informacion_basic_producto(data)
-        sectionHero.style='block';
-        
+        sectionHero.style = 'block';
+
     }
 
-    else if (url.hash==='#allProducts'){
+    else if (url.hash === '#allProducts') {
         console.log('allProducts')
-        informacion_basic_producto(data,'all-products')
+        informacion_basic_producto(data, 'all-products')
     }
 
     else if (url.hash.includes('productoID')) {
         console.log('producto')
         buttonRegister.style.display = 'none'
-        mostrarProducto(url,data)
+        mostrarProducto(url, data)
 
     }
 
-    else if  (url.hash==='#register') {
+    else if (url.hash === '#register') {
         console.log('register')
         buttonRegister.style.display = 'none'
+        contenedorBuscar.style.display = 'none'
         register()
     }
 
-    else if (url.hash==='#login') {
+    else if (url.hash === '#login') {
         console.log('login')
+        contenedorBuscar.style.display = 'none'
         buttonLogin.style.display = 'none'
         buttonRegister.style.display = 'inline-block'
         login()
 
     }
 
+    else if (url.hash.includes('Dashboard')) {
+        console.log('Dashboard')
+        contenedorBuscar.style.display = 'none'
+
+
+        buttonLogin.textContent = 'salir'
+        buttonLogin.className = 'buttonSalir'
+        buttonRegister.style.display = 'none'
+
+
+        sectionContact.style.display = 'none'
+
+        if (url.hash.includes('True')) {
+            console.log('administrador')
+            dashboard('administrador')
+
+        }
+        else {
+            console.log('usuario')
+            dashboard('usuario')
+            const buttonAddProduct = document.querySelector('.btn-agregar-producto')
+            buttonAddProduct.style.display = 'none'
+        }
+
+        // desde la validación del login se llama la funcion dashboard que ejecuta el codigo 
+    }
+
+    else if (url.hash === '#addProduct') {
+        console.log('addproduct')
+
+        contenedorBuscar.style.display = 'none'
+
+        buttonRegister.style.display = 'none'
+
+        buttonLogin.textContent = 'salir'
+        buttonLogin.className = 'buttonSalir'
+
+        sectionContact.style.display = 'none'
+
+        addProduct() //crea el formulario
+
+        // VALIDACIONES de las  imagenes y el formulario
+        validarArrastrarImagen()
+        inputFileValidate()
+
+    }
+
+    // const sesionActive = localStorage.getItem('sesionActive')
+    // // console.log(sesionActive)
+
+    // if (sesionActive==null){
+    //   window.location.href = './login.html'
+    // }
+
+
+
+
 
 }
-
-
-
-
-// else if (url.pathname.includes('/inicio-administrador.html')) {
-//     informacion_productos_administrador(informacion_start_wars)
-//     informacion_productos_administrador(informacion_consolas)
-//     informacion_productos_administrador(informacion_diversos)
-
