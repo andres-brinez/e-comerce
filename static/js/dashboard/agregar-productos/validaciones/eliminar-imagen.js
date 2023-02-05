a148557623e426b7eead13c9bc3a07f03466a0e1
Andres Briñez <brinezlopez08@gmail.com>
2023-02-04 22:07:14 -0500

    crear la opción para eliminar la imagen seleccionada

---
+++ static/js/dashboard/agregar-productos/validaciones/eliminar-imagen.js
@@ -0,0 +1,14 @@
+export function eliminar_imagen() {
+    const opciones_agregar_archivo = document.querySelector('.opcion-agregar-producto')
+
+    opciones_agregar_archivo.classList.remove('oculto')
+    opciones_agregar_archivo.classList.add('mostrar')
+
+
+    const contenedor = document.querySelector('.contenedor-imagen');
+    //eliminar  imagen
+    contenedor.childNodes[0].remove()
+    //eliminar  la el texto
+    contenedor.childNodes[0].remove()
+
+}
