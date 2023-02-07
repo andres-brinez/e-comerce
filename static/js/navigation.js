a65be581139951063036017fcf9885c9b615ca08
Andres Briñez <brinezlopez08@gmail.com>
2023-02-07 15:14:30 -0500

    agregar la sesion de contacto y header

--- static/js/navigation.js
+++ static/js/navigation.js
@@ -3,6 +3,7 @@ import { data } from "../../Information/data.js";
 import { informacion_basic_producto } from "./product-card-basic/index.js";
 import { mostrarProducto } from "./InformationProducto/index.js";
 import { hero } from "./components/hero.js";
+import { setionContact } from "./components/contactSetion.js";
 
 import { register } from "./register/index.js";
 import { login } from "./login/index.js";
@@ -10,6 +11,10 @@ import { login } from "./login/index.js";
 import { dashboard } from "./dashboard/index.js";
 import { addProduct } from "./dashboard/agregar-productos/index.js";
 import { editProduct } from "./dashboard/crudProducts/update/index.js";
+
+import { getLocalStoreSesion } from "./utils/getSesionActive.js";
+
+import { headerBasic,headerLogin,headerRegister,headerDashboard} from "./components/header.js";
 
 //new URL es una funcion ya echa que nos trae la url
 const url = new URL(window.location);
@@ -26,109 +31,106 @@ window.onload = () => {
     navigation()
 }
 
+const isLogeado= getLocalStoreSesion()
+
 function navigation() {
     // home
     if (url.hash === '') {
         console.log('inicio')
+        headerBasic()
         hero()
-
         informacion_basic_producto(data)
-        // sectionHero.style = 'block';
+        setionContact()
 
     }
 
     else if (url.hash === '#allProducts') {
         console.log('allProducts')
+        headerBasic()
         informacion_basic_producto(data, 'all-products')
     }
 
     else if (url.hash.includes('productoID')) {
         console.log('producto')
-        buttonRegister.style.display = 'none'
+        headerBasic()
         mostrarProducto(url, data)
 
     }
 
     else if (url.hash === '#register') {
         console.log('register')
-        buttonRegister.style.display = 'none'
-        contenedorBuscar.style.display = 'none'
+        headerRegister()
         register()
     }
 
     else if (url.hash === '#login') {
         console.log('login')
-        contenedorBuscar.style.display = 'none'
-        buttonLogin.style.display = 'none'
-        buttonRegister.style.display = 'inline-block'
+        headerLogin()
         login()
 
     }
 
-    else if (url.hash.includes('Dashboard')) {
-        console.log('Dashboard')
-        contenedorBuscar.style.display = 'none'
+    // se debe estár logeado para acceder  a la página
+    else {
 
+        headerDashboard()
+        
+        // se hacen validaciones si esta logeado
 
-        buttonLogin.textContent = 'salir'
-        buttonLogin.className = 'buttonSalir'
-        buttonRegister.style.display = 'none'
-
-
-        sectionContact.style.display = 'none'
-
-        if (url.hash.includes('True')) {
-            console.log('administrador')
-            dashboard('administrador')
+        if (url.hash.includes('Dashboard')) {
+            if(isLogeado==null){
+                alert('no puedes acceder a esta secion, logeate ')
+                window.location.href = ''
+            }
+            else{
+                console.log('Dashboard')
+                
+                if (url.hash.includes('True')) {
+                    console.log('administrador')
+                    dashboard('administrador')
+        
+                }
+                else {
+                    console.log('usuario')
+                    dashboard('usuario')
+                    
+                }
+        
+                // desde la validación del login se llama la funcion dashboard que ejecuta el codigo 
+            }
+           }
+    
+        else if (url.hash === '#addProduct') {
+            if(isLogeado==null){
+                alert('no puedes acceder a esta secion, logeate ')
+                window.location.href = ''
+            }
+            else{
+                console.log('addproduct')
+                addProduct()
+            }
 
         }
-        else {
-            console.log('usuario')
-            dashboard('usuario')
-            const buttonAddProduct = document.querySelector('.btn-agregar-producto')
-            buttonAddProduct.style.display = 'none'
+    
+        else if (url.hash.includes('edit')) {
+            if(isLogeado==null){
+                alert('no puedes acceder a esta secion, logeate ')
+                window.location.href = ''
+            }
+            else{
+            
+                if (isLogeado.rol=='Administrador'){
+                    console.log('editProduct')
+
+                    editProduct()
+
+                }
+                else{
+                    alert('no tienes acceso  ')
+                    window.location.href = ''
+                }
+            
+            }
         }
-
-        // desde la validación del login se llama la funcion dashboard que ejecuta el codigo 
     }
-
-    else if (url.hash === '#addProduct') {
-        console.log('addproduct')
-
-        contenedorBuscar.style.display = 'none'
-
-        buttonRegister.style.display = 'none'
-
-        buttonLogin.textContent = 'salir'
-        buttonLogin.className = 'buttonSalir'
-
-        sectionContact.style.display = 'none'
-
-        addProduct()
-
-        
-
-    }
-
-    else if (url.hash.includes('edit')) {
-        console.log('editProduct')
-
-        contenedorBuscar.style.display = 'none'
-
-        buttonRegister.style.display = 'none'
-
-        buttonLogin.textContent = 'salir'
-        buttonLogin.className = 'buttonSalir'
-
-        sectionContact.style.display = 'none'
-        editProduct()
-
-        
-    }
-
-    // if (sesionActive==null){
-    //   window.location.href = './login.html'
-    // }
-
-
 }
