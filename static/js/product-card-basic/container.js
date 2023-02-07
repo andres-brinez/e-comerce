e5bb68223479f59da8b0557fc41d08defa597f54
Andres Briñez <brinezlopez08@gmail.com>
2023-02-07 12:01:17 -0500

    cambiar el container

--- static/js/product-card-basic/container.js
+++ static/js/product-card-basic/container.js
@@ -1,25 +1,25 @@
 // se encarga de  la estructura del contendor de los productos 
 
 function createContainerArticle(key){
-    const containerMain = document.createElement('article');
-    containerMain.classList.add("article-type-product")
-    containerMain.setAttribute("category", key)
-    containerMain.innerHTML=
-    
-    `
+    const container= document.createElement('section')
+    container.classList.add("section-products")
 
+    container.innerHTML=
+    
+    ` <article class="article-type-product" category="${key}">
         <div class="descripcion">
           <h2 class="title-type-product">${key}</h2>
           <div class="see-all">
             <a href="index.html#allProducts">ver todo</a><img src="./static/img/section-products/flecha.svg" alt="flecha" class="flecha" />
-          </div>
+        </div>
         </div>
         <div class="contenedor-produtos " ${key}> 
         </div>
-    
+      </article>  
+
     `
 
-    sectionProducts.appendChild(containerMain)
+    main.appendChild(container)
 
 }
 
