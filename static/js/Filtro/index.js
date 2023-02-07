5a944d72f2ef2753f0ef2f702c1bda0d4e7d535c
Andres Briñez <brinezlopez08@gmail.com>
2023-02-07 15:08:35 -0500

    encapsular el co+ódigo en una funcion

--- static/js/Filtro/index.js
+++ static/js/Filtro/index.js
@@ -2,42 +2,37 @@ import { findProduct } from "./findProduct.js";
 import { AddProductStructure } from "./estructura.js";
 
 
-lupaIcon.addEventListener("click", ()=>{
+function filtroIcon (){
+    const lupaIcon=document.querySelector(".buscador-lupa")
+    lupaIcon.addEventListener("click", ()=>{
 
-    const coincidencias = findProduct()
+        const coincidencias = findProduct()
+    
+        if (coincidencias.length == 0){
+            Swal
+                .fire({
+                    title: "Lo sentimos",
+                    text: `No hay resultados para la busqueda ${campo_filtrar.value} `,
+                    icon: "info",
+                    button: "Ok!",
+                })
+                // .then(resultado => {
+                //     //cuando cierre el mensaje en ok
+                //     if (resultado.value) {
+                //         location.reload();
+                        
+                //     } 
+                // });
+        }
+        else {
+            //Crea la estructura en html para ver  los productos que coinciden con lo ingresado 
+            AddProductStructure(coincidencias)
+        }
+    
+    })
+}
 
-    if (coincidencias.length == 0){
-        Swal
-            .fire({
-                title: "Lo sentimos",
-                text: `No hay resultados para la busqueda ${campo_filtrar.value} `,
-                icon: "info",
-                button: "Ok!",
-            })
-            // .then(resultado => {
-            //     //cuando cierre el mensaje en ok
-            //     if (resultado.value) {
-            //         location.reload();
-                    
-            //     } 
-            // });
-    }
-    else {
-        //Crea la estructura en html para ver  los productos que coinciden con lo ingresado 
-        AddProductStructure(coincidencias)
-    }
-
-})
-
-
-
-
-
-
-    //         
-    //     }
-    // }
-    // }
+export  {filtroIcon}
 
    
 
