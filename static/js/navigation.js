84333410abebf60056c8f1beecc0f6b006732663
Andres Briñez <brinezlopez08@gmail.com>
2023-02-07 16:43:23 -0500

    agregar alertas  para cuando no se está logeado

--- static/js/navigation.js
+++ static/js/navigation.js
@@ -16,6 +16,7 @@ import { getLocalStoreSesion } from "./utils/getSesionActive.js";
 
 import { headerBasic,headerLogin,headerRegister,headerDashboard} from "./components/header.js";
 
+import { alertNoLogeado } from "./utils/alerts.js";
 //new URL es una funcion ya echa que nos trae la url
 const url = new URL(window.location);
 
@@ -76,71 +77,81 @@ function navigation() {
     }
 
     // se debe estár logeado para acceder  a la página
-    else {
-        console.log(isLogeado)
-        headerDashboard()
-        
-        // se hacen validaciones si esta logeado
-
-        if (url.hash.includes('Dashboard')) {
-            document.title='Dashboard'
-
-            if(isLogeado==null){
-                alert('no puedes acceder a esta secion, logeate ')
-                window.location.href = ''
-            }
-            else{
-                console.log('Dashboard')
-                
-                if (url.hash.includes('True')) {
-                    console.log('administrador')
-                    dashboard('administrador')
-        
-                }
-                else {
-                    console.log('usuario')
-                    dashboard('usuario')
-                    
-                }
-        
-                // desde la validación del login se llama la funcion dashboard que ejecuta el codigo 
-            }
-           }
     
-        else if (url.hash === '#addProduct') {
-            document.title='Agregar producto'
-            if(isLogeado==null){
-                alert('no puedes acceder a esta secion, logeate ')
-                window.location.href = ''
-            }
-            else{
-                console.log('addproduct')
-                addProduct()
-            }
 
+    else if (url.hash.includes('Dashboard')) {
+        document.title='Dashboard'
+
+        if(isLogeado==null){
+            alertNoLogeado()
         }
+        else{
+            console.log('Dashboard')
+            headerDashboard()
+            
+            if (url.hash.includes('True')) {
+                console.log('administrador')
+                dashboard('administrador')
     
-        else if (url.hash.includes('edit')) {
-            document.title='Editar Producto'
-            if(isLogeado==null){
-                alert('no puedes acceder a esta secion, logeate ')
-                window.location.href = ''
+            }
+            else {
+                console.log('usuario')
+                dashboard('usuario')
+                
+            }
+    
+            // desde la validación del login se llama la funcion dashboard que ejecuta el codigo 
+        }
+        }
+
+    else if (url.hash === '#addProduct') {
+        document.title='Agregar producto'
+
+        if(isLogeado==null){
+            alertNoLogeado()
+        }
+        else{
+            console.log('addproduct')
+            addProduct()
+            headerDashboard()
+        }
+
+    }
+
+    else if (url.hash.includes('edit')) {
+        document.title='Editar Producto'
+        if(isLogeado==null){
+            alertNoLogeado()
+            
+        }
+        else{
+        
+            if (isLogeado.rol=='Administrador'){
+                console.log('editProduct')
+                headerDashboard()
+                editProduct()
+
             }
             else{
-            
-                if (isLogeado.rol=='Administrador'){
-                    console.log('editProduct')
-
-                    editProduct()
-
-                }
-                else{
-                    alert('no tienes acceso  ')
-                    window.location.href = ''
-                }
+                alert('no tienes acceso  ')
+                window.location.href = ''
             }
         }
     }
+    
+    // pagina  no encontrada
+    else {
+        
+          Swal.fire({
+            icon: 'error',
+            title: 'Oops...',
+            text: 'Página no encontrada',
+            showConfirmButton: false,
+            footer: '<a href="">Ir a  inicio</a>'
+
+          })
+    }
+    
 
     
 
