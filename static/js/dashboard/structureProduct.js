0c01cebeb117d1499e4f74bdd10ceb3f40964eb4
Andres Briñez <brinezlopez08@gmail.com>
2023-02-07 12:27:02 -0500

    Cambiar la estructura del contenido dependiendo el rol del usuario

--- static/js/dashboard/structureProduct.js
+++ static/js/dashboard/structureProduct.js
@@ -1,25 +1,51 @@
+import { getLocalStoreSesion} from '../utils/getSesionActive.js'
+
 function structureProduct(product,category){
     const contenedor = document.createElement("div")   
     contenedor.classList.add("contenedor-producto")
 
-    const contenido = `
-            <div class="contenedor-image">
-                <div class="contenedor-eliminar"> 
-                    <img src="./static//img/section-products/icon-eliminar.svg" class="icon-eliminar"/>
-                </div>
-                <div class="contenedor-editar">             
-                <a target='_blank' href="#edit?id=${product.id}&category=${category}">
-                <img src="./static/img/section-products/icon-editar.svg" class="icon-editar"/>
-                </a>
-                    
-                </div>
-                 <img src=${product.imagen}  class="imagen-producto" id= ${product.id} />
-            </div>
+    const sesionActive = getLocalStoreSesion() // trae la  información de la sesion que está activa
+
+    let contenido=''
+
+    // dependiendo el rol se  muestra  la  información
     
-            <p class="name-product">${product.nombre}</p>
-            <p class="price-product">${product.precio}</p>
-            <a href="./producto.html?id=${product.id}" class="see-product" target="_blank">Ver producto</a>
-            `
+    if(sesionActive.rol === "User"){
+        // no se da la opción para eliminar y editar
+    
+        contenido=`
+    <div class="contenedor-image">
+        <img src=${product.imagen}  class="imagen-producto" id= ${product.id} />
+    </div>
+
+    <p class="name-product">${product.nombre}</p>
+    <p class="price-product">${product.precio}</p>
+    <a href="./producto.html?id=${product.id}" class="see-product" target="_blank">Ver producto</a>
+    `
+    }
+    else {
+        // se da la  opción de editar y  eliminar  
+        contenido = `
+        <div class="contenedor-image">
+            <div class="contenedor-eliminar"> 
+                <img src="./static//img/section-products/icon-eliminar.svg" class="icon-eliminar"  />
+            </div>
+            <div class="contenedor-editar">             
+            <a target='_blank' href="#edit?id=${product.id}&category=${category}">
+            <img src="./static/img/section-products/icon-editar.svg" class="icon-editar"/>
+            </a>
+                
+            </div>
+            <img src=${product.imagen}  class="imagen-producto" id= ${product.id} />
+        </div>
+
+        <p class="name-product">${product.nombre}</p>
+        <p class="price-product">${product.precio}</p>
+        <a href="./producto.html?id=${product.id}" class="see-product" target="_blank">Ver producto</a>
+        `
+
+    }
+
     contenedor.innerHTML = contenido
  
     return contenedor
