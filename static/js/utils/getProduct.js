5fe5df14b3be0d55785e1f27b32fa8a5fe368cb3
Andres Briñez <brinezlopez08@gmail.com>
2023-02-07 16:11:51 -0500

    ..

--- static/js/utils/getProduct.js
+++ static/js/utils/getProduct.js
@@ -1,22 +1,22 @@
 import { data } from "../../../Information/data.js";
 
 // obtiene el producto con los datos de  la url
-function getProduct(){
-    
+function getProduct() {
+
     // obtener  los valores de  la url
-    const url = new URL(window.location); 
-  
+    const url = new URL(window.location);
+
     // obtener la  informacion del producto desde la url 
-    
-    const category= url.hash.split('&')[1].split('=')[1].replace('%20',' ')
-    const idUrlProduct= url.hash.split('&')[0].split('=')[1]
-    
-    for   (let i=0; i< data[category].length; i++) {
+
+    const category = url.hash.split('&')[1].split('=')[1].replace('%20', ' ')
+    const idUrlProduct = url.hash.split('&')[0].split('=')[1]
+
+    for (let i = 0; i < data[category].length; i++) {
         if (data[category][i].id == parseFloat(idUrlProduct)) {
             return data[category][i]
         }
-        
+
     }
 }
 
-export  {getProduct}
+export { getProduct }
